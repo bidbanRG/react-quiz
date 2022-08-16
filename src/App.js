0aa5a19e7ccb56128b3619b345925286a00ee717
Bidesh <bideshbanerjee444@gmail.com>
2022-08-16 15:06:34 +0530

    progressBar added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
+import ProgressBar from 'react-bootstrap/ProgressBar';
 import "./App.css";
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 const questions = [
   {
     questionText: "Qual o idiomafalado no Brasil?",
@@ -45,12 +46,19 @@ function App() {
   const [showScore, setShowScore] = useState(false);
   const [score, setScore] = useState(0);
   const [currentQuestion, setCurrentQuestion] = useState(0);
-
+  const [Points,setPoints] = useState(0);
+  const [correctBar,setCorrectBar] = useState(0);
+  const [wrongBar,setWrongBar] = useState(0);
   function handleAnswer(isCorrect) {
     if (isCorrect) {
+      setPoints((point) => point + 5)
+      setCorrectBar((bar) => bar + 25);
       setScore(score + 1);
     }
-
+    else{
+      setPoints((point) => point - 4)
+      setWrongBar((bar) => bar + 25);
+    }
     const nextQuestion = currentQuestion + 1;
     if (nextQuestion < questions.length) {
       setCurrentQuestion(nextQuestion);
@@ -61,6 +69,22 @@ function App() {
 
   return (
     <div className="app">
+      <section>
+       <p> Points  </p>
+       </section>
+       <section>
+       <p> {Points} </p>
+      </section> 
+       <header className = 'header'>
+         <div className = 'answerBar'>  
+           <h5> Right Ans </h5>
+          <ProgressBar variant="info"  now={correctBar}  className = 'progress_bar' />
+         </div>
+         <div className = 'answerBar'>
+            <h5> Wrong Ans </h5>
+         <ProgressBar variant="danger" now={wrongBar} className = 'progress_bar' />
+         </div>
+       </header>
       {showScore ? (
         <div className="score-section">
           Você pontuou {score} de {questions.length}
@@ -69,7 +93,7 @@ function App() {
         <>
           <div className="question-section">
             <div className="question-count">
-              <span>Questão {currentQuestion + 1}</span>/{questions.length}
+              <span> Questão {currentQuestion + 1}  </span>/{questions.length}
             </div>
             <div className="question-text">
               {questions[currentQuestion].questionText}
@@ -90,6 +114,8 @@ function App() {
           </div>
         </>
       )}
+
+    {!showScore &&  <p> Note: Right ans is 5 points and Wrong ans leads to -4 points </p>}
     </div>
   );
 }
